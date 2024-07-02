f7f262dc75fff15aa35471251335e3d4bfff2a5a
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-01 23:35:05 -0400

    Text validation Days 2  through Python programming

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ def check_text(text):
 user_input = input("Matin kiriting: ")
 corrected_text = check_text(user_input)
 print(f"Tuzatilgan matn: {corrected_text}")
+from textblob import TextBlob
+
+def check_text(text):
+    blob = TextBlob(text)
+    corrected_text = blob.correct()
+    return corrected_text
+
+# Misol uchun, foydalanuvchidan matn olish va uni tekshirish
+user_input = input("Matin kiriting: ")
+corrected_text = check_text(user_input)
+print(f"Tuzatilgan matn: {corrected_text}")
