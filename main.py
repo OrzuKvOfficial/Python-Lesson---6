e28aef6fd3429a7206a43eedb4cd6213fffdb461
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-09 23:04:15 -0400

    Working with lists

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-import zipfile
-import os
+# List yaratish
+my_list = [1, 2, 3, 4, 5]
 
-def zip_files(directory, output_zip):
-    with zipfile.ZipFile(output_zip, 'w') as zipf:
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, directory))
+# List elementlariga murojaat qilish
+print(my_list[0])  # Birinchi element
+print(my_list[-1])  # So'nggi element
 
-# Misol uchun, "my_folder" papkasidagi barcha fayllarni "output.zip" ga siqish
-directory_to_zip = 'my_folder'
-output_zip_file = 'output.zip'
-zip_files(directory_to_zip, output_zip_file)
+# Listga yangi element qo'shish
+my_list.append(6)
+print(my_list)
 
-print(f"{output_zip_file} fayli muvaffaqiyatli yaratildi.")
+# Listdan elementni olib tashlash
+my_list.remove(3)
+print(my_list)
+
+# Listni birlashtirish
+another_list = [7, 8, 9]
+combined_list = my_list + another_list
+print(combined_list)
+
+# Listni tartiblash
+my_list.sort()
+print(my_list)
+my_list.sort(reverse=True)
+print(my_list)
+
+# Listni aylantirish (iteratsiya)
+for element in my_list:
+    print(element)
+
+# List comprehension
+squares = [x**2 for x in range(10)]
+print(squares)
+
+# List uzunligini aniqlash
+length_of_list = len(my_list)
+print(length_of_list)
+
+# List elementlarini tekshirish
+if 3 in my_list:
+    print("3 ro'yxatda bor")
+else:
+    print("3 ro'yxatda yo'q")
+
+# Listdan kesmalar olish
+sub_list = my_list[1:4]
+print(sub_list)
